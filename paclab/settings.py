39a0169e4e79f4379ad58d279959a7154ae4a21e
Rebecca Brunner <brunner@bgsu.edu>
2018-10-25 09:56:26 -0400

    Change database_url line to line in app documentation

--- paclab/settings.py
+++ paclab/settings.py
@@ -79,11 +79,7 @@ WSGI_APPLICATION = 'paclab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 
 
 # Password validation
