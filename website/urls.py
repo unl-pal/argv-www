fc726dd6e8cfedd7d0529add074b7a262aefd9f6
Rebecca Brunner <brunner@bgsu.edu>
2019-02-08 11:27:08 -0500

    Remove project selection url

--- website/urls.py
+++ website/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('logout/', views.logoutView, name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('editprofile/', views.profile, name='editProfile'),
-    path('project/selection/', views.project_selection, name='project_selection'),
     path('password/reset/', auth_views.PasswordResetView.as_view(email_template_name='website/password_reset_email.html', success_url=reverse_lazy('website:password_reset_done'), template_name='website/reset/passwordReset.html'), name='password_reset'),
     path('password/reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='website/reset/passwordResetDone.html'), name='password_reset_done'),
     path('password/reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('website:password_reset_complete'), template_name='website/reset/passwordResetConfirm.html'), name='password_reset_confirm'),
