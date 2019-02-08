d54e4a1ba68b7c59c2db9ed23a7c09d38c9dcdbe
Rebecca Brunner <brunner@bgsu.edu>
2019-02-08 12:54:08 -0500

    Fix bug for __str__ return value

--- website/models.py
+++ website/models.py
@@ -99,7 +99,7 @@ class Selection(models.Model):
     name = models.CharField(max_length=200, default="")
 
     def __str__(self):
-        return 'self.name'
+        return self.name
 
 class Filter(models.Model):
     name = models.CharField(max_length=200, default="")
