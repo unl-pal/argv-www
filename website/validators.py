0394e98720b50b1ada4c8195fc00050e41e6409d
Robert Dyer <rdyer@bgsu.edu>
2019-02-11 15:06:47 -0500

    fixes #25 - verifies the GH token is valid

--- website/validators.py
+++ website/validators.py
@@ -1,3 +1,4 @@
+import requests
 from django.core.exceptions import ValidationError
 from django.conf import settings
 
@@ -5,6 +6,11 @@ def validate_file_size(value):
     filesize = value.size
     
     if filesize > settings.MAX_FILE_UPLOAD:
-        raise ValidationError("The maximum file size for profile photos is " + str(settings.MAX_FILE_UPLOAD))
-    else:
-        return value
+        raise ValidationError('The maximum file size for profile photos is ' + str(settings.MAX_FILE_UPLOAD))
+    return value
+
+def validate_gh_token(token):
+    response = requests.get('https://api.github.com/rate_limit', headers={'Authorization': 'token ' + token})
+    if 'message' in response.json():
+        raise ValidationError('The GitHub Personal Token provided is invalid')
+    return token
