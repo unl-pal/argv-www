c1af3516cc8b850fcd55b843dfaef5a1ba849b94
Rebecca Brunner <brunner@bgsu.edu>
2019-02-13 11:16:24 -0500

    Remove form control

--- website/forms.py
+++ website/forms.py
@@ -57,7 +57,6 @@ class FilterDetailForm(forms.Form):
     value = forms.CharField(
         max_length=1000,
         widget=forms.TextInput(attrs={
-            'class' : 'form-control',
             'placeholder' : 'Enter value'
         }),
         required=True)
