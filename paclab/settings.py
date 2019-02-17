a4f015c999b7e50cbaf02d8f12059efeb036f244
Robert Dyer <rdyer@bgsu.edu>
2019-02-17 04:43:11 -0500

    allow configuring default from email

--- paclab/settings.py
+++ paclab/settings.py
@@ -138,6 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
 # These need to be set in the .env file
 EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
