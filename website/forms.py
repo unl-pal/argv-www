503491c12cf5423420d02f0451f56a27723ba73a
Robert Dyer <rdyer@bgsu.edu>
2019-02-17 01:53:35 -0500

    ensure a bad form submission cant produce huge images, or cropped all-black images, etc

--- website/forms.py
+++ website/forms.py
@@ -1,9 +1,6 @@
-from django.conf import settings
 from django.contrib.auth.models import User
 from django import forms
-from PIL import Image
 from .models import Profile
-from .validators import validate_file_size
 
 class UserForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -35,40 +32,18 @@ class UserFormRegister(forms.ModelForm):
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
 
 class ProfileForm(forms.ModelForm):
-    class PhotoInput(forms.widgets.ClearableFileInput):
-        template_name = 'website/photoinput.html'
-
-    x = forms.FloatField(required=False, widget=forms.HiddenInput())
-    y = forms.FloatField(required=False, widget=forms.HiddenInput())
-    width = forms.FloatField(required=False, widget=forms.HiddenInput())
-    height = forms.FloatField(required=False, widget=forms.HiddenInput())
-    photo = forms.ImageField(validators=[validate_file_size], widget=PhotoInput())
-
     class Meta:
+        class PhotoInput(forms.widgets.ClearableFileInput):
+            template_name = 'website/photoinput.html'
+
         model = Profile
         fields = ['photo', 'bio', 'token', 'sharetoken']
         labels = {
-            'photo' : 'Photo',
-            'bio' : 'Bio',
+            'bio' : 'Biography',
             'token' : 'Github Personal Access Token',
             'sharetoken' : 'Allow using token for system jobs'
         }
         widgets = {
             'token': forms.TextInput(attrs={'size': 40}),
+            'photo': PhotoInput(),
         }
-
-    def save(self):
-        profile = super(ProfileForm, self).save()
-
-        x = self.cleaned_data.get('x')
-        y = self.cleaned_data.get('y')
-        w = self.cleaned_data.get('width')
-        h = self.cleaned_data.get('height')
-
-        if x and y and w and h:
-            image = Image.open(profile.photo)
-            cropped_image = image.crop((x, y, w + x, h + y))
-            resized_image = cropped_image.resize(settings.THUMBNAIL_SIZE, Image.LANCZOS)
-            resized_image.save(profile.photo.path)
-
-        return profile
