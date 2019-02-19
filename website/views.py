8252c2d005f3f36d7cb479505eebdc1fada3927b
Rebecca Brunner <brunner@bgsu.edu>
2019-02-19 15:34:38 -0500

    Remove passing heading as parameter

--- website/views.py
+++ website/views.py
@@ -98,7 +98,6 @@ def profile(request):
 
 def project_selection(request):
     template_name = 'website/create_normal.html'
-    heading_message = 'Project Selection'
     if request.method == 'GET':
         p_form = ProjectSelectionForm(request.GET or None)
         formset = FilterFormSet(request.GET or None)
@@ -129,7 +128,6 @@ def project_selection(request):
     return render(request, template_name, {
         'p_form' : p_form,
         'formset': formset,
-        'heading': heading_message,
     })
 
 def data_default(request):
