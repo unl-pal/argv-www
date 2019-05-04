4de2e871facdcf8984bcbf2865f785953bb5cfda
Robert Dyer <rdyer@bgsu.edu>
2019-05-04 02:02:56 -0400

    refactor to rename activate -> verify for email verification

--- website/tokens.py
+++ website/tokens.py
@@ -7,4 +7,4 @@ class TokenGenerator(PasswordResetTokenGenerator):
             six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_active)
         )
 
-account_activation_token = TokenGenerator()
+email_verify_token = TokenGenerator()
