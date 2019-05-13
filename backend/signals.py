0f2a385f70cdd2fcb1bab1661a100cd01c80e9d8
Rebecca Brunner <brunner@bgsu.edu>
2019-05-13 12:53:41 -0400

    Add documentation for commands

--- backend/signals.py
+++ backend/signals.py
@@ -4,6 +4,16 @@ from django.core import management
 from website.models import Filter
 from .models import Backend
 
+'''Enable Backend
+
+Arguments:
+-> sender: the object triggering the signal
+-> instance: the object the function performs operations on
+-> created: a boolean value showing if the sender was newly created
+-> kwargs: any additional arguments
+Triggers when enabling a backend on the admin page
+If the object has not been newly installed, load all associated filters and enable them
+'''
 @receiver(post_save, sender=Backend)
 def enableBackend(sender, instance, created, **kwargs):
     if not created:
