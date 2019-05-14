cef22c6ca77017d54574614aee4929be00c98092
Rebecca Brunner <brunner@bgsu.edu>
2019-05-14 13:05:02 -0400

    Rename enable to enabled

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Backend(models.Model):
     name = models.CharField(max_length=255)
-    enable = models.BooleanField(default=False)
+    enabled = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
