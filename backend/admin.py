fe69873e76bf600e3c2796ede080b482971ab66a
Rebecca Brunner <brunner@bgsu.edu>
2019-05-14 13:17:48 -0400

    Change backend to readonly on admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Backend
 
-admin.site.register(Backend)
+class BackendAdmin(admin.ModelAdmin):
+    model = Backend
+    readonly_fields = ['name','enabled']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(Backend, BackendAdmin)
