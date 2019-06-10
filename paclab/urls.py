d4350204e5ce665a74409d3d75939895c9c9269a
Robert Dyer <rdyer@bgsu.edu>
2019-06-10 15:00:57 -0400

    rename default admin url

--- paclab/urls.py
+++ paclab/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pi-admin/', admin.site.urls),
     re_path(r'^hijack/', include('hijack.urls', namespace='hijack')),
     path('', include('website.urls'))
 ]
