0de58a6d1c56b581847511e2ddf875765bdcf5e0
Robert Dyer <rdyer@bgsu.edu>
2019-09-11 16:32:29 -0400

    add --dry-run and --no-poll options as well as ability to process specific slugs

--- backend/management/commands/runpuller.py
+++ backend/management/commands/runpuller.py
@@ -16,22 +16,44 @@ Runs a boa puller in the background to grab unprocessed project selections from 
 class Command(BaseCommand):
     help = 'Runs a boa puller in the background to grab unprocessed project selections from database'
     
-    def handle(self, *args, **options):
-        while(True):
-            selectors = ProjectSelector.objects.all().exclude(processed=PROCESSED)
-            for selector in selectors:
-                selector.processed = ONGOING
-                selector.save()
-                filters = selector.filterdetail_set.all()
-                backends = []
-                for pfilter in filters:
-                    associated_backend = str(pfilter.pfilter.associated_backend)
-                    if associated_backend not in backends:
-                        backends.append(associated_backend)
+    def add_arguments(self, parser):
+        parser.add_argument('slug', nargs='*', help='Specific project selection slug(s) to process', type=str)
+        parser.add_argument('--dry-run', help='Perform a dry-run (don\'t change the database)', action='store_true')
+        parser.add_argument('--no-poll', help='Perform one round of processing instead of polling', action='store_true')
 
-                for backend in backends:
-                    modname = associated_backend + '_backend.runner'
-                    backend = importlib.import_module(modname)
-                    runner = backend.Runner(selector)
-                    process = multiprocessing.Process(target=runner.run, args=())
-            time.sleep(3)
+    def handle(self, *args, **options):
+        self.dry_run = options['dry_run']
+        self.no_poll = options['no_poll']
+
+        for slug in options['slug']:
+            self.process_selection(ProjectSelector.objects.get(slug=slug,processed=READY))
+
+        if len(options['slug']) == 0:
+            while True:
+                selectors = ProjectSelector.objects.filter(processed=READY)
+                for selector in selectors:
+                    self.process_selection(selector)
+
+                if self.no_poll:
+                    break
+                time.sleep(3)
+
+    def process_selection(self, selector):
+        self.stdout.write('processing ProjectSelection: ' + selector.slug)
+        if not self.dry_run:
+            selector.processed = ONGOING
+            selector.save()
+
+        filters = selector.filterdetail_set.all()
+        backends = set()
+        for pfilter in filters:
+            associated_backend = str(pfilter.pfilter.associated_backend)
+            backends.add(associated_backend)
+
+        for backend in backends:
+            modname = backend + '_backend.runner'
+            backend = importlib.import_module(modname)
+            runner = backend.Runner(selector)
+            self.stdout.write('    -> calling backend: ' + modname)
+            #process = multiprocessing.Process(target=runner.run, args=())
+            runner.run()
