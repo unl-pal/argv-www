d5d89e2c06918f9589d1ef146ed130e7149a61df
Robert Dyer <rdyer@bgsu.edu>
2019-09-28 17:46:51 -0400

    processed time should be nullable

--- website/models.py
+++ website/models.py
@@ -143,7 +143,7 @@ class Project(models.Model):
     url = models.URLField(max_length=1000)
     host = models.CharField(max_length=1000, null=True, blank=True)
     path = models.CharField(max_length=5000, null=True, blank=True)
-    datetime_processed = models.DateTimeField(auto_now=True)
+    datetime_processed = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.url
