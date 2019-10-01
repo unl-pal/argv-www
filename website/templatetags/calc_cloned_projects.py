1cc8b5c82a406db9532c847eb2f98d740758c9a4
Rebecca Brunner <brunner@bgsu.edu>
2019-10-01 13:59:28 -0400

    Clean template tag

--- website/templatetags/calc_cloned_projects.py
+++ website/templatetags/calc_cloned_projects.py
@@ -9,33 +9,3 @@ def calc_cloned_projects(selector):
         if project.host and project.datetime_processed != '' and project.path:
             count += 1
     return count
-
-@register.simple_tag
-def url_replace(request, field, value):
-    query_string = request.GET.copy()
-    query_string[field] = value
-
-    return query_string.urlencode()
-
-@register.filter(name='proper_paginate')
-def proper_paginate(paginator, current_page, neighbors=5):
-    width = 2 * neighbors
-    if paginator.num_pages > width:
-        start_index = max(1, current_page - neighbors)
-        end_index = min(paginator.num_pages, current_page + neighbors)
-
-        if end_index < start_index + width:
-            end_index = start_index + width
-        elif start_index > end_index - width:
-            start_index = end_index - width
-
-        if start_index < 1:
-            end_index -= start_index
-            start_index = 1
-        elif end_index > paginator.num_pages:
-            start_index -= (end_index - paginator.num_pages)
-            end_index = paginator.num_pages
-
-        page_list = [f for f in range(start_index, end_index + 1)]
-        return page_list[:(width + 1)]
-    return paginator.page_range
