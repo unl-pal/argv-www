7f0be422ed5cfd5be39f6accde734e4ec399da4c
Robert Dyer <rdyer@bgsu.edu>
2019-10-23 16:49:22 -0400

    fix crash

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter(name='calc_finished_projects')
 def calc_finished_projects(selector):
     count = 0
-    for project in selector.project.all():
-        if project.transformedproject_set.first().path:
+    for project in selector.project.filter(path__isnull=False):
+        if project.transformedproject_set.filter(path__isnull=False):
             count += 1
     return count
