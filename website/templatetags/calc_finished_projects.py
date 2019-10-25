2cc3d22a4bcf360176892835afb16c70489f0afa
Robert Dyer <rdyer@bgsu.edu>
2019-10-24 22:51:57 -0400

    better performing transformed count

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -1,10 +1,10 @@
 from django import template
+from website.models import TransformedProject
 
 register = template.Library()
 
 @register.filter(name='calc_finished_projects')
 def calc_finished_projects(selector):
-    count = 0
-    for project in selector.project.filter(path__isnull=False):
-        count += project.transformedproject_set.filter(path__isnull=False).count()
-    return count
+    qs1 = selector.project.filter(path__isnull=False).values_list('id')
+    qs2 = TransformedProject.objects.filter(path__isnull=False).values_list('project')
+    return qs1.intersection(qs2).count()
