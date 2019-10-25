3febc86f3fd41ad2fc6402bbb2a271069dae1262
Robert Dyer <rdyer@bgsu.edu>
2019-10-24 23:16:53 -0400

    show remaining transforms

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -8,3 +8,9 @@ def calc_finished_projects(selector):
     qs1 = selector.project.filter(path__isnull=False).values_list('id')
     qs2 = TransformedProject.objects.filter(path__isnull=False).values_list('project')
     return qs1.intersection(qs2).count()
+
+@register.filter(name='calc_remaining_projects')
+def calc_remaining_projects(selector):
+    qs1 = selector.project.filter(path__isnull=False).values_list('id')
+    qs2 = TransformedProject.objects.filter(host__isnull=False).values_list('project')
+    return qs1.difference(qs2).count()
