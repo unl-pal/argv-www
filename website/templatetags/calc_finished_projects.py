722d1cd0f4d935af179dc7237b5e1e83b54d4a8a
Robert Dyer <rdyer@bgsu.edu>
2019-10-24 21:50:18 -0400

    fix typo

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -6,5 +6,5 @@ register = template.Library()
 def calc_finished_projects(selector):
     count = 0
     for project in selector.project.filter(path__isnull=False):
-        count += project.transformedproject_set.filter(path__isnull=False).count():
+        count += project.transformedproject_set.filter(path__isnull=False).count()
     return count
