fcdd26f7db67dc87ccbb5fdb55b335179dfc9af9
Robert Dyer <rdyer@bgsu.edu>
2019-10-29 14:23:29 -0400

    better handling of recovery

--- backend/management/commands/runcloner.py
+++ backend/management/commands/runcloner.py
@@ -53,53 +53,60 @@ class Command(BaseCommand):
             project.host = socket.gethostname()
             project.save()
 
-        host = project.url[project.url.index("://") + 3:]
-        project_name = host[host.index("/") + 1:]
-        host = host[:host.index("/")]
+        host = project.url[project.url.index('://') + 3:]
+        project_name = host[host.index('/') + 1:]
+        project_base = project_name[0:project_name.index('/')] if '/' in project_name else project_name
+        project_parent = project_name[0:project_name.rindex('/')] if '/' in project_name else ''
+        host = host[:host.index('/')]
 
-        tmp = '/tmp/' + project_name[:project_name.rindex('/')]
-        repo_root = config('REPO_PATH')
-        if repo_root[-1:] != '/':
-            repo_root += '/'
-        repo_root += host
-        path = repo_root + '/' + project_name
+        repo_root = os.path.join(config('REPO_PATH'), host)
+        path = os.path.join(repo_root, project_name)
+        tmp = '/tmp/'
 
         if self.verbosity >= 3:
             self.stdout.write('    -> root: ' + repo_root)
-            self.stdout.write('    -> tmp: ' + tmp)
             self.stdout.write('    -> path: ' + path)
+            self.stdout.write('    -> name: ' + project_name)
+            self.stdout.write('    -> base: ' + project_base)
 
         if os.path.exists(path):
             self.stdout.write('    -> SKIPPING: already exists: ' + project_name)
             if not self.dry_run:
-                project.path = path
+                project.path = os.path.join(host, project_name)
                 project.datetime_processed = timezone.now()
                 project.save()
             return
 
         # clone
-        p = subprocess.Popen(['git', 'clone', '--depth', '1', 'https://foo:bar@' + host + '/' + project_name, project_name], cwd='/tmp/', stdout=subprocess.PIPE if self.verbosity < 2 else None, stderr=subprocess.PIPE if self.verbosity < 2 else None)
+        p = subprocess.Popen(['git', 'clone', '--depth', '1', 'https://foo:bar@' + host + '/' + project_name, project_name], cwd=tmp, stdout=subprocess.PIPE if self.verbosity < 3 else None, stderr=subprocess.PIPE if self.verbosity < 3 else None)
         if self.verbosity >= 2:
             self.stdout.write('    -> process id: ' + str(p.pid))
         p.wait()
+
+        src_dir = os.path.join(tmp, project_name)
 
         if not self.dry_run:
             if p.returncode == 0:
                 if not os.path.exists(repo_root):
                     self.stdout.write('root does not exist, creating: ' + repo_root)
-                    os.mkdir(repo_root, 0o755)
-                if self.verbosity >= 2:
-                    self.stdout.write('moving ' + tmp + ' -> ' + repo_root)
-                # move repo to final location
-                shutil.move(tmp, repo_root)
+                    os.makedirs(repo_root, 0o755, True)
 
-                project.path = host + '/' + project_name
-                project.datetime_processed = timezone.now()
-                project.save()
+                # move repo to final location
+                parent_dir = os.path.join(repo_root, project_parent)
+                if self.verbosity >= 2:
+                    self.stdout.write('moving ' + src_dir + ' -> ' + parent_dir)
+                if not os.path.exists(parent_dir):
+                    os.makedirs(parent_dir, 0o755, True)
+                shutil.move(src_dir, parent_dir)
+
+                project.path = os.path.join(host, project_name)
             else:
-                project.datetime_processed = timezone.now()
-                project.save()
+                if self.verbosity >= 2:
+                    self.stdout.write('failed to clone ' + project_name)
+
+            project.datetime_processed = timezone.now()
+            project.save()
 
         # cleanup temp files
-        if os.path.exists(tmp):
-            shutil.rmtree(tmp)
+        if os.path.exists(src_dir):
+            shutil.rmtree(src_dir)
