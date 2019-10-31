27014054be86f30784c9ff694315d082a4ce180a
Robert Dyer <rdyer@bgsu.edu>
2019-10-31 11:34:23 -0400

    optimize project detail tag usage

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -3,20 +3,18 @@ from website.models import TransformedProject
 
 register = template.Library()
 
-@register.filter(name='calc_finished_projects')
+@register.simple_tag(name='calc_finished_projects')
 def calc_finished_projects(selector):
     qs1 = selector.project.filter(path__isnull=False).values_list('id')
     qs2 = TransformedProject.objects.filter(path__isnull=False).values_list('project')
     return qs1.intersection(qs2).count()
 
-@register.filter(name='calc_remaining_projects')
+@register.simple_tag(name='calc_remaining_projects')
 def calc_remaining_projects(selector):
     qs1 = selector.project.filter(path__isnull=False).values_list('id')
     qs2 = TransformedProject.objects.filter(host__isnull=False).values_list('project')
     return qs1.difference(qs2).count()
 
-@register.filter(name='calc_remaining_percent')
-def calc_remaining_percent(selector):
-    done = calc_finished_projects(selector)
-    remain = calc_remaining_projects(selector)
+@register.simple_tag(name='calc_remaining_percent')
+def calc_remaining_percent(done, remain):
     return round(100.0 * done / remain if remain > 0.0 else 0.0, 2)
