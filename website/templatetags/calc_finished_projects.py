cc5c9510eebc664e0f295c1fe2097553e5b28c84
Rebecca Brunner <brunner@bgsu.edu>
2019-12-06 13:08:24 -0500

    Remove extra space

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -12,7 +12,7 @@ def calc_finished_projects(selector):
 @register.simple_tag(name='calc_finished_transformedprojects')
 def calc_finished_transformedprojects(transformer):
     qs1 = transformer.project_selector.project.filter(path__isnull=False).values_list('id')
-    qs2 = transformer.transformed_projects.filter(path__isnull=False).values_list('project') 
+    qs2 = transformer.transformed_projects.filter(path__isnull=False).values_list('project')
     return qs1.intersection(qs2).count()
 
 @register.simple_tag(name='calc_remaining_projects')
