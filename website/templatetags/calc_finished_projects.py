034be269c1bf768cdb5e7dc15879b8521b0acfe7
Rebecca Brunner <brunner@bgsu.edu>
2019-12-06 13:06:04 -0500

    Fixes #78 - Get tags working

--- website/templatetags/calc_finished_projects.py
+++ website/templatetags/calc_finished_projects.py
@@ -12,7 +12,7 @@ def calc_finished_projects(selector):
 @register.simple_tag(name='calc_finished_transformedprojects')
 def calc_finished_transformedprojects(transformer):
     qs1 = transformer.project_selector.project.filter(path__isnull=False).values_list('id')
-    qs2 = transformer.transformed_projects.filter(path__isnull=False).values_list('project')
+    qs2 = transformer.transformed_projects.filter(path__isnull=False).values_list('project') 
     return qs1.intersection(qs2).count()
 
 @register.simple_tag(name='calc_remaining_projects')
@@ -23,8 +23,8 @@ def calc_remaining_projects(selector):
 
 @register.simple_tag(name='calc_remaining_transformedprojects')
 def calc_remaining_transformedprojects(transformer):
-    qs1 = transformer.project_selector.project.filter(path__isnull=False).values_list('id')
-    qs2 = transformer.transformed_projects.filter(host__isnull=False).values_list('project')
+    qs1 = transformer.project_selector.project.filter(path__isnull=True).values_list('id')
+    qs2 = transformer.transformed_projects.filter(host__isnull=True).values_list('project')
     return qs1.difference(qs2).count()
 
 @register.simple_tag(name='calc_remaining_percent')
