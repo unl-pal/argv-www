2152f72d2e2a81cabb854407b88e3c56b9922147
Robert Dyer <rdyer@unl.edu>
2020-09-14 15:53:39 -0500

    Add README.md file into each generated benchmark zip Fixes #92

---
+++ website/downloads.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import subprocess
+import tempfile
+
+def generate_zip(in_paths, tmpdir, zipfile_path, cwd, readme = None):
+    os.mkdir(tmpdir, 0o755)
+
+    # generate README.md file and add to the zipfile
+    if readme:
+        readmepath = tempfile.mkdtemp()
+
+        readmefile = os.path.join(readmepath, 'README.md')
+        f = open(readmefile, 'w')
+        f.write(readme)
+        f.close()
+
+        p = subprocess.Popen(['zip', '-g', '-@', '-b', tmpdir, zipfile_path], cwd=readmepath, stdin=subprocess.PIPE)
+        p.communicate(input=str.encode('README.md\n'))
+        p.stdin.close()
+        p.wait()
+
+        shutil.rmtree(readmepath)
+
+    try:
+        for host in in_paths:
+            s = ''
+            for path in in_paths[host]:
+                s = s + path + '\n'
+            p = subprocess.Popen(['zip', '-r', '-g', '-@', '-b', tmpdir, zipfile_path],
+                                    cwd=os.path.join(cwd, host),
+                                    stdin=subprocess.PIPE)
+            p.communicate(input=str.encode(s))
+            p.stdin.close()
+            p.wait()
+    except:
+        if os.path.exists(zipfile_path):
+            shutil.rmtree(zipfile_path)
+    finally:
+        if os.path.exists(tmpdir):
+            shutil.rmtree(tmpdir)
