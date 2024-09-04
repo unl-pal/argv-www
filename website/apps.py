9cc17bf6b65af2256a31f6a227c65f0dae8738b5
Samuel W. Flint <swflint@flintfam.org>
2024-09-04 17:51:12 -0500

    Set automatic field types

--- website/apps.py
+++ website/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     name = 'website'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         import website.signals
