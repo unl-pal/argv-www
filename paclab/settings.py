29f9a68e89474a9cdf827e98f1452531797d6d68
Robert Dyer <rdyer@unl.edu>
2024-09-07 16:11:37 -0500

    fix path bug with media

--- paclab/settings.py
+++ paclab/settings.py
@@ -18,7 +18,7 @@ import logging.config
 from django.utils.log import DEFAULT_LOGGING
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
